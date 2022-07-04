6c119c9d0e86fe54167d75911c0fc5a8b172b3d5
Safina <safikaravina@gmail.com>
2022-07-04 05:26:21 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,14 +5,9 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 1 (Success)
- *
- *     */
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 
